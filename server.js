a13bcbc805ed1f2888fb7ca6cc9d3c17196697d1
asyncaditya <asyncaditya@outlook.com>
2023-09-28 16:04:33 +0530

    added post end point

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express()
 
+let arr = []
+
 app.get('/',(req,res)=>{
     res.send("success")
+})
+
+app.post('/insert',(req,res)=>{
+    let random = Math.floor(Math.random()*10)
+    arr.push(random)
+    res.send(arr)
 })
 
 app.listen(2000,()=>{
